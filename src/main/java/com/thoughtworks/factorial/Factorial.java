ebd5036609ccfd91cfb63c5eb67aeb461f91d382
Dan <dlafeir@umich.edu>
2015-06-13 16:10:55 -0500

    fifth test passed

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -1,7 +1,12 @@
 package com.thoughtworks.factorial;
 
+import java.util.IllegalFormatCodePointException;
+
 public class Factorial {
     public Integer compute(int i) {
+       if(i < 0){
+           throw new IllegalArgumentException();
+       }
         return (i == 0) ? 1 : (i*compute(i-1));
     }
 }
